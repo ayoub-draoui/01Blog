4bb194f8d8a6a2dbe13ad03f72e8faa62d535016
ayoub-draoui <mr.stefan.draoui@gmail.com>
2025-12-09 17:35:42 +0100

    baby-steps

--- backend/src/main/java/_blog/demo/security/CustomUserDetailsService.java
+++ backend/src/main/java/_blog/demo/security/CustomUserDetailsService.java
@@ -1,5 +1,22 @@
 package _blog.demo.security;
 
-public class CustomUserDetailsService {
+
+import _blog.demo.model.User;
+import _blog.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+   @Autowired
+    private UserRepository userRepository;
+   @Override
+   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+       User user = userRepository.findByUsername(username)
+       .orElseThrow(()-> new UsernameNotFoundException("username not found :" + username));
+       return new CustomUserDetails(user);
+    }
     
 }
--- backend/src/main/java/_blog/demo/security/JwtUtil.java
+++ backend/src/main/java/_blog/demo/security/JwtUtil.java
@@ -1,5 +1,60 @@
 package _blog.demo.security;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+@Component
 public class JwtUtil {
-    
+
+    private String SECRET_KEY = "123456";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public String generateToken(CustomUserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails.getUsername());
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        final Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        long expiration = 1000 * 6000;
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public boolean validateToken(String token, CustomUserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
